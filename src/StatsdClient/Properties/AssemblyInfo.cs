d7f9fa58e9461b6d197b48a8cbb0c1617aa8aebd
Kyle West <kwest2123@yahoo.com>
2015-08-03 11:53:27 -0400

    Rename to StatsdClient.HighPerformance due to nuget conflicts

--- src/StatsdClient/Properties/AssemblyInfo.cs
+++ src/StatsdClient/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StatsD.HighPerformance")]
+[assembly: AssemblyTitle("StatsdClient.HighPerformance")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StatsD.HighPerformance")]
+[assembly: AssemblyProduct("StatsdClient.HighPerformance")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
